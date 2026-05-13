93d7bb7853ab4845c96588664781d96a00b4362c
Walter <walter94.amaya@gmail.com>
2026-05-13 10:56:09 -0600

    Finalizacion de vista de Registros, conectado a la BD

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,11 @@
+from urllib import request
+
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 """"""
 def inicio_sesion(request):
@@ -27,6 +31,35 @@ def home(request):
 
 
 def register(request):
-    
-    return render(request, 'register.html')
 
+    if request.method == "POST":
+
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+
+        # Validar contraseñas
+        if password != confirm_password:
+            return render(request, "register.html", {
+                "error": "Las contraseñas no coinciden"
+            })
+
+        # Validar si usuario ya existe
+        if User.objects.filter(username=username).exists():
+            return render(request, "register.html", {
+                "error": "El usuario ya existe"
+            })
+
+        # Crear usuario
+        usuario = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        usuario.save()
+
+        return redirect("index")
+
+    return render(request, "register.html")
